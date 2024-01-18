c95c7e80eaef74a4ae311ae700da5e732f9b870a
BasuRathour99 <145312778+BasuRathour99@users.noreply.github.com>
2024-01-18 22:02:50 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    private static char[][] board = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+    };
+
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        displayBoard();
+        playGame();
+    }
+
+    private static void displayBoard() {
+        System.out.println("  1   2   3");
+        System.out.println("1 " + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+        System.out.println("  ----------");
+        System.out.println("2 " + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+        System.out.println("  ----------");
+        System.out.println("3 " + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+    }
+
+    private static void playGame() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        while (true) {
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+
+            if (isValidMove(row, col)) {
+                board[row][col] = currentPlayer;
+                displayBoard();
+
+                if (checkWin()) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    break;
+                } else if (isBoardFull()) {
+                    System.out.println("It's a draw!");
+                    break;
+                }
+
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ';
+    }
+
+    private static boolean checkWin() {
+    
+        return (checkRows() || checkColumns() || checkDiagonals());
+    }
+
+    private static boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals() {
+        return (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+               (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer);
+    }
+
+    private static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
